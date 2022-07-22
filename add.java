56ec87157e7c306186f2f9fce45329b955e435f7
M <mohit26102002@gmail.com>
2022-07-22 12:30:39 +0530

    Added a .java file

---
+++ add.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] arg) {
+        int age;
+        float b;
+        char c = 'A';
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+        b = sc.nextFloat();
+
+        System.out.println(c);
+        System.out.println(b);
+
+        System.out.println(age);
+        sc.close();
+
+    }
+    
+}
